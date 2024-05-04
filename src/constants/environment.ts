d247b260d37a66e769bee853e5043b13aa3d0375
Dev-Aligator <devaligator1004@gmail.com>
2024-05-05 01:43:16 +0700

    switch to pythonanywhere api url

--- src/constants/environment.ts
+++ src/constants/environment.ts
@@ -1,4 +1,4 @@
 export const environmentVariable = {
-    // baseUrl: "https://aligator.pythonanywhere.com/",
-    baseUrl: "http://127.0.0.1:8000",
+    baseUrl: "https://aligator.pythonanywhere.com",
+    // baseUrl: "http://127.0.0.1:8000",
 }
