23ef732e3f782c1f3e6d71e15692c49b67086cee
Dev-Aligator <devaligator1004@gmail.com>
2024-05-14 01:52:33 +0700

    Disable React.Strict mode on production to prevent fetching data twice

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.sass";
 import { HashRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
